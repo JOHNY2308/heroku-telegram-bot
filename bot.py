9bb4d64e54254349d80e8cc398951255c6c388d3
JOHNY2308 <35346486+JOHNY2308@users.noreply.github.com>
2018-01-15 22:43:21 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,20 +1,68 @@
-# -*- coding: utf-8 -*-
-import redis
 import os
 import telebot
-# import some_api_lib
-# import ...
+from telebot import types
+import const
+from geopy.distance import vincenty
 
 # Example of your code beginning
 #           Config vars
 token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
 
 #       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+bot = telebot.TeleBot(token)
+
+markup_menu = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+btn_address = types.KeyboardButton('🏪 Адреса магазинов', request_location=True)
+btn_payment = types.KeyboardButton('💵 Способы оплаты')
+btn_delivery = types.KeyboardButton('🚗 Способы доставки')
+markup_menu.add(btn_address, btn_payment, btn_delivery)
+
+markup_inline_payment = types.InlineKeyboardMarkup(row_width=1)
+btn_in_cash = types.InlineKeyboardButton('Наличные', callback_data='cash')
+btn_in_card = types.InlineKeyboardButton('По карте', callback_data='card')
+btn_in_invoice = types.InlineKeyboardButton('Банковский перевод', callback_data='invoice')
+
+markup_inline_payment.add(btn_in_cash, btn_in_card, btn_in_invoice)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Здаров мудила я твой нигер", reply_markup=markup_menu)
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    if message.text == "🚗 Способы доставки":
+        bot.reply_to(message, "Хуй тебе в рот, а не доставка!", reply_markup=markup_menu)
+    elif message.text == "💵 Способы оплаты":
+        bot.reply_to(message, "В наших секс-шопах доступны следующие способы оплаты ",
+                     reply_markup=markup_inline_payment)
+    else:
+        bot.reply_to(message, message.text, reply_markup=markup_menu)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['location'])
+def magazin_location(message):
+    lon = message.location.longitude
+    lat = message.location.latitude
+
+    distance = []
+    for m in const.MAGAZINS:
+        result = vincenty((m['latm'], m['lonm']), (lat, lon)).kilometers
+        distance.append(result)
+    index = distance.index(min(distance))
+
+    bot.send_message(message.chat.id, 'Ближайший к Вам магазин')
+    bot.send_venue(message.chat.id, const.MAGAZINS[index]['latm'], const.MAGAZINS[index]['lonm'],
+                   const.MAGAZINS[index]['title'], const.MAGAZINS[index]['address'])
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def call_back_payment(call):
+    if call.data == 'cash':
+        bot.send_message(call.message.chat.id, text="""
+        Наличная оплата, производится в рублях, в кассе магазина""", reply_markup=markup_inline_payment)
+
+
+bot.polling()
